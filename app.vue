d8895f4616270917c90bdf01e49de85a947a43a7
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-08-18 08:16:02 -0600

    Adding project's base code

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <nav>
+      <img src="https://res.cloudinary.com/dkvtpo8w1/image/upload/v1660831973/MJ%20Portafolio/Epadilla/EP_Logo.png" alt="">
+      <ul>
+        <li>Portafolio</li>
+        <li>Escríbeme</li>
+      </ul>
+    </nav>
+  </div>
+</template>
