b924ae618ac1b499b1c9c92552abd107e0a9a4df
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-08-18 09:17:38 -0600

    Adding tailwind and google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  modules: ['@nuxtjs/tailwindcss'],
+  buildModules: ['@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      Roboto: {
+        wght: [300, 400]
+      }
+    }
+  }
 })
