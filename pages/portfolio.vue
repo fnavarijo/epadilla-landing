91d45b95c4f73527ea1eec9889027dbf2b878043
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-08-23 00:39:43 -0600

    feat: Adding header

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,3 +1,8 @@
 <template>
-  <article class="mt-32 max-w-7xl mx-auto px-8">jeje</article>
+  <article class="mt-32 max-w-7xl mx-auto px-8">
+    <header class="flex items-center">
+      <h1 class="text-5xl text-black uppercase">Bodas</h1>
+      <span class="h-3 w-3 bg-black rounded-full ml-3"></span>
+    </header>
+  </article>
 </template>
