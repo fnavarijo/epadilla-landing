d2f08bc6fd4329140b93c67476a4cb2a4abc1d0a
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-08-22 21:03:29 -0600

    feat: Handling color of menu options

--- app.vue
+++ app.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="relative">
-    <AppNavbar class="absolute w-full" />
+    <AppNavbar class="absolute w-full z-10" is-white />
 
-    <div class="w-screen h-screen bg-slate-400">
+    <div class="w-screen h-screen bg-black bg-opacity-20 absolute"></div>
+    <div class="w-screen h-screen bg-[url('https://res.cloudinary.com/dkvtpo8w1/image/upload/v1661219355/MJ%20Portafolio/Epadilla/pexels-saulo-zayas-335887.jpg')] bg-no-repeat bg-cover bg-center">
       
     </div>
   </div>
--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,32 +1,44 @@
+<script setup>
+defineProps({
+  isWhite: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
 <template>
-  <!-- TODO: Move it to layout. Should not be absolute always -->
-  <div class="px-16">
+  <div class="nav-menu px-16" :class="[isWhite ? 'text-white' : 'text-black']">
     <nav class="grid grid-cols-2 items-center">
-      <img class="w-24" src="https://res.cloudinary.com/dkvtpo8w1/image/upload/v1660831973/MJ%20Portafolio/Epadilla/EP_Logo.png" alt="">
+      <img class="w-24" src="https://res.cloudinary.com/dkvtpo8w1/image/upload/v1660831973/MJ%20Portafolio/Epadilla/EP_Logo_white.png" alt="">
       <ul class="flex justify-self-end space-x-8">
-        <li class="nav-item uppercase tracking-widest group">
+        <li class="nav-item uppercase tracking-widest group" :class="[isWhite ? 'before:bg-white' : 'before:bg-black']">
           <span>Portafolio</span>
           <div class="nav-item-submenu h-0 overflow-y-hidden min-w-[10ch] relative transition-all duration-100 ease-linear">
-            <ul class="pr-2 py-2 space-y-3 text-right border-r-2 border-black absolute right-0 bottom-0">
+            <ul
+              class="pr-2 py-2 space-y-3 text-right border-r-2 absolute right-0 bottom-0"
+              :class="[isWhite ? 'border-white' : 'border-black']"
+            >
               <li class="text-sm">Bodas</li>
               <li class="text-sm">General</li>
             </ul>
           </div>
         </li>
-        <li class="nav-item uppercase tracking-widest">Escríbeme</li>
+        <li class="nav-item uppercase tracking-widest" :class="[isWhite ? 'before:bg-white' : 'before:bg-black']">Escríbeme</li>
       </ul>
     </nav>
   </div>
 </template>
 
 <style scoped>
+
 .nav-item {
   @apply relative transition-all duration-200 ease-fast-in;
 }
 
 .nav-item::before {
   content: '';
-  @apply h-[2px] w-0 bg-black absolute -top-1 transition-all duration-100 ease-linear;
+  @apply h-[2px] w-0 absolute -top-1 transition-all duration-100 ease-linear;
 }
 
 .nav-item:hover::before {
