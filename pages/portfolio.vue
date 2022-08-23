720c962a14d0958c1b54913b73c69651eacc8d70
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-08-23 00:50:16 -0600

    feat: Adding mobile size

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,7 +1,7 @@
 <template>
   <article class="mt-32 max-w-7xl mx-auto px-8">
     <header class="flex items-center">
-      <h1 class="text-5xl text-black uppercase">Bodas</h1>
+      <h1 class="text-3xl md:text-5xl text-black uppercase">Bodas</h1>
       <span class="h-3 w-3 bg-black rounded-full ml-3"></span>
     </header>
   </article>
