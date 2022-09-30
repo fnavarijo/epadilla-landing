041312ac548f82357ffc63289a60d6a489c0e910
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-09-29 23:28:05 -0600

    Navigation on gallery click

--- pages/portfolio/[name]/[id].vue
+++ pages/portfolio/[name]/[id].vue
@@ -38,10 +38,14 @@ const galleryImages = getImagesChunks(imagesToLoad, 5);
 <template>
   <article class="mt-32 max-w-7xl mx-auto px-8 pb-8">
     <header class="flex items-center">
-      <h1 class="text-3xl md:text-5xl text-black uppercase">{{ params.name }}</h1>
+      <NuxtLink :to="`/portfolio/${params.name}`">
+        <h1 class="text-3xl md:text-5xl text-black uppercase">{{ params.name }}</h1>
+      </NuxtLink>
       <span class="h-3 w-3 bg-black rounded-full ml-3"></span>
       <span class="ml-8 text-2xl uppercase">Lopez Perez</span>
     </header>
-    <GalleryMasonry v-for="images in galleryImages" :images="images" />
+    <div class="mt-8">
+      <GalleryMasonry v-for="images in galleryImages" :images="images" />
+    </div>
   </article>
 </template>
