6b56660bc0be915d14928c10cc23cf24e3b74976
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-09-30 00:15:55 -0600

    Fixing visibility of image

--- components/Gallery/GalleryViewer.vue
+++ components/Gallery/GalleryViewer.vue
@@ -6,12 +6,14 @@ import chevron from '~/assets/images/chevron.png';
 <template>
   <!-- TODO: disable scroll -->
   <div class="fixed bg-black/60 w-screen h-screen top-0 left-0 flex justify-center items-center">
-    <article class="w-4/5 min-h-[75%] grid grid-cols-viewer gap-4">
+    <!-- TODO: Overflow hidden is not the solution. Need to make the iamge responsive to space -->
+    <article class="w-4/5 min-h-[75%] max-h-[90%] overflow-hidden grid grid-cols-viewer gap-4">
       <div class="h-full flex items-center">
         <img :src="chevron" class="h-6 w-6 transform rotate-90" alt="" />
       </div>
       <div class="bg-white">
         <img
+          class="h-full"
           src="https://res.cloudinary.com/dkvtpo8w1/image/upload/v1662790818/MJ%20Portafolio/Epadilla/pexels-roberto-nickson-2559941.jpg"
           alt=""
         />
