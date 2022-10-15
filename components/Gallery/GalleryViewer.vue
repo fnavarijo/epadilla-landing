c23d05d7fe24b9141635cef5fe588509aa8991d3
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-10-14 23:56:44 -0600

    fix: Fixing dynamic image on space

--- components/Gallery/GalleryViewer.vue
+++ components/Gallery/GalleryViewer.vue
@@ -40,22 +40,21 @@ function viewPreviousImage() {
 <template>
   <!-- TODO: disable scroll -->
   <!-- TODO: Because is fixed, the padding counts behind the scroll bar -->
-  <div
-    class="px-8 fixed bg-black/60 w-screen h-screen top-0 left-0 flex justify-center items-center"
-  >
-    <article class="w-full md:w-[90%] min-h-[75%] max-h-[90%] viewer-grid">
+  <div class="p-8 fixed bg-black/60 w-screen h-screen top-0 left-0">
+    <article class="w-full viewer-grid">
       <button
         class="action-button justify-self-end self-center grid-area-close"
         @click="$emit('close')"
       >
         <img :src="closeIcon" class="h-6 w-6" alt="Close dialog" />
       </button>
-      <!-- // TODO: Check how it behaves with vertical images -->
-      <img
-        class="h-full object-contain w-full max-w-4xl grid-area-content place-self-center"
-        :src="images[imageViewedIndex]"
-        alt=""
-      />
+      <div class="w-full h-full flex justify-center">
+        <img
+          class="h-full object-contain w-full grid-area-content place-self-center"
+          :src="images[imageViewedIndex]"
+          alt=""
+        />
+      </div>
       <div class="flex place-self-center grid-area-controls space-x-12">
         <button class="action-button" @click="viewPreviousImage">
           <img :src="chevron" class="h-6 w-6 transform rotate-90" alt="" />
@@ -70,7 +69,11 @@ function viewPreviousImage() {
 
 <style>
 .viewer-grid {
-  @apply grid grid-rows-viewer grid-cols-1 gap-y-4;
+  @apply grid grid-rows-viewer grid-cols-1 gap-y-4 h-full;
+}
+
+.viewer-grid > * {
+  min-height: 0;
 }
 
 .grid-area-close {
