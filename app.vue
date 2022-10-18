7ca695414a456af776e6fe2636b6b6913474fc07
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-10-18 09:03:58 -0600

    Adding base mobile menu

--- app.vue
+++ app.vue
@@ -2,6 +2,7 @@
 import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import AppNavbar from '~/components/AppNavbar.vue';
+import AppNavbarMobile from '~/components/AppNavbarMobile.vue';
 
 const route = useRoute();
 
@@ -17,11 +18,11 @@ watch(
 <template>
   <div>
     <AppNavbar
-      class="w-full max-w-7xl block"
+      class="w-full max-w-7xl hidden md:block"
       :class="[isHomePage ? 'absolute z-10 left-1/2 -translate-x-1/2' : 'mx-auto']"
       :is-white="isHomePage"
     />
-    <!-- <div class="absolute md:hidden">this is mobile</div> -->
+    <AppNavbarMobile class="absolute md:hidden z-20 top-0 left-0" />
 
     <NuxtPage />
   </div>
---
+++ components/AppNavbarMobile.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+import MenuIcon from '~/assets/images/menu.png';
+import CloseIcon from '~/assets/images/close.png';
+
+// TODO: Duplicated
+const props = defineProps({
+  isWhite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const isOpen = ref(false);
+
+const logoImage = computed(() =>
+  props.isWhite
+    ? 'https://res.cloudinary.com/dkvtpo8w1/image/upload/v1660831973/MJ%20Portafolio/Epadilla/EP_Logo_white.png'
+    : 'https://res.cloudinary.com/dkvtpo8w1/image/upload/v1660831973/MJ%20Portafolio/Epadilla/EP_Logo.png'
+);
+</script>
+
+<template>
+  <div class="p-4 pr-6 w-full flex place-items-center justify-between">
+    <div>
+      <NuxtLink to="/"><img class="w-20" :src="logoImage" alt="" /></NuxtLink>
+    </div>
+    <div>
+      <button @click="isOpen = true"><img :src="MenuIcon" alt="" width="32" /></button>
+    </div>
+    <div class="absolute">
+      <div
+        class="fixed top-0 left-0 h-screen w-screen bg-black/80 z-40"
+        :class="[isOpen ? 'visible' : 'invisible']"
+      ></div>
+      <div
+        class="fixed top-0 right-0 min-h-screen w-2/3 bg-white z-50 p-4 pr-6 flex flex-col items-end"
+        :class="[isOpen ? 'translate-x-0' : 'translate-x-full']"
+      >
+        <button @click="isOpen = false">
+          <img :src="CloseIcon" alt="" width="32" />
+        </button>
+        <nav class="mt-12">
+          <ul class="uppercase tracking-widest">
+            <li>Portafolio</li>
+            <ul class="text-right pr-2 space-y-2 border-r-2 mt-2 border-black">
+              <li class="text-sm">
+                <NuxtLink @click="isOpen = false" to="/portfolio/bodas">Bodas</NuxtLink>
+              </li>
+              <li class="text-sm">
+                <NuxtLink @click="isOpen = false" to="/portfolio/general">Proyectos</NuxtLink>
+              </li>
+            </ul>
+          </ul>
+        </nav>
+        <NuxtLink
+          class="absolute uppercase tracking-widest bottom-4 self-center"
+          @click="isOpen = false"
+          to="/contact"
+        >
+          Escríbeme
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
