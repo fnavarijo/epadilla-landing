adb8a80be80573f937bda2c1bc9bdc92b4407c48
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-10-19 23:45:11 -0600

    feat: Enabling mobile menu

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
 import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import AppNavbar from '~/components/AppNavbar.vue';
-// import AppNavbarMobile from '~/components/AppNavbarMobile.vue';
+import AppNavbarMobile from '~/components/AppNavbarMobile.vue';
 
 const route = useRoute();
 
@@ -22,7 +22,7 @@ watch(
       :class="[isHomePage ? 'absolute z-10 left-1/2 -translate-x-1/2' : 'mx-auto']"
       :is-white="isHomePage"
     />
-    <!-- <AppNavbarMobile class="absolute md:hidden z-20 top-0 left-0" /> -->
+    <AppNavbarMobile class="absolute md:hidden z-20 top-0 left-0" />
 
     <NuxtPage />
   </div>
