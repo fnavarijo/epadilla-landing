351b5813db50e0b5ede2f5d9f6119512396b55c7
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2023-01-11 00:36:03 -0600

    feat: Adding pagination

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import { Carousel, Slide, Pagination } from 'vue3-carousel';
 import 'vue3-carousel/dist/carousel.css';
 
 // TODO: Optimize image
@@ -18,7 +18,26 @@ const images = [
         <Slide v-for="(image, index) in images" :key="index">
           <img class="h-screen w-full object-cover object-center" :src="image" alt="Header image" />
         </Slide>
+
+        <template #addons>
+          <Pagination />
+        </template>
       </Carousel>
     </ClientOnly>
   </div>
 </template>
+
+<style>
+.carousel__pagination {
+  @apply absolute bottom-4 w-full z-20;
+}
+
+.carousel__pagination-button::after {
+  background-color: #a3a3a3;
+}
+
+.carousel__pagination-button:hover::after,
+.carousel__pagination-button--active::after {
+  background-color: white;
+}
+</style>
